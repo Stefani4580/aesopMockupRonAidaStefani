6bb0f2462fee856966adeb46497ef741bc3a79d8
Student_MA102 <aidarous12@gmail.com>
2020-07-18 17:05:30 -0400

    added Shop.js

---
+++ src/components/Shop.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class Shop extends Component {
+    render() {
+        return (
+            <div className= "Shop">
+                <div className="skin">
+                    <navbar></navbar>
+                </div>
+                <div className="hair"></div>
+                <div className="body-hand"></div>
+                <div className="fragrance"></div>
+                <div className="home"></div>
+
+                
+            </div>
+        )
+    }
+}
+
