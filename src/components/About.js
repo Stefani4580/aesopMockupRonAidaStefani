77eec1b27fbfa1908882ab37bbb29e4cf520f598
Ronald McGuire <ronaldmcguire@PSADMINs-MacBook-Air-86.local>
2020-07-18 15:48:20 -0400

    here i will add a link and this is where i will make the rest of the words

--- src/components/About.js
+++ src/components/About.js
@@ -7,6 +7,13 @@ export default class About extends Component {
 
             <h4> Our Story </h4>
             <h2> We offer skin, hair and body care formulations created with meticulous attention to detail, and with efficacy and sensory pleasure in mind. </h2>
+<br></br>
+
+<h2> Aesop was established in 1987. We are headquartered in Melbourne, and have offices and stores in many parts of the world. </h2>
+
+<h3>Our objective has always been to formulate skin, hair and body care products of the finest quality; we investigate widely to source plant-based and laboratory-made ingredients, and use only those with a proven record of safety and efficacy. In each of our unique stores, informed consultants are pleased to introduce the Aesop range and to guide your selections.
+
+</h3>
                 
             </div>
         )
