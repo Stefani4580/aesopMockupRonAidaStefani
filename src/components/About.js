162f48f92ab8a54a107a0531c1cf175209388d52
Ronald McGuire <ronaldmcguire@PSADMINs-MacBook-Air-86.local>
2020-07-18 14:46:02 -0400

    I am building the words and what it will say

--- src/components/About.js
+++ src/components/About.js
@@ -4,8 +4,12 @@ export default class About extends Component {
     render() {
         return (
             <div>
+
+            <h4> Our Story </h4>
+            <h2> We offer skin, hair and body care formulations created with meticulous attention to detail, and with efficacy and sensory pleasure in mind. </h2>
                 
             </div>
         )
     }
 }
+
