14f88edbf1ca5d0210d45d29710d4394aed2d181
Stefani Baker <stefanibaker@MacBook-Air-4.local>
2020-07-21 20:19:21 -0400

    Fix font on first class

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ import About from "./About";
 import Stores from "./Stores";
 import Search from "./Search";
 import Card from "react-bootstrap/Card";
-import CardGroup from "react-bootstrap/CardGroup"
+import CardGroup from "react-bootstrap/CardGroup";
 
 export default class Home extends React.Component {
   render() {
@@ -51,43 +51,63 @@ export default class Home extends React.Component {
               </ul>
             </nav>
             <div class="row">
-
-            <div class="col-5">
-              <h1>For the Body</h1>
-              <h2>An expression of care</h2>
-              <p>Aesop formulations offer the body deserving care, to cleanse, replenish, and nourish skin. Each product is a sensory pleasure to use with its own delightful aroma.</p>
+              <div class="col-5">
+                <h1>For the Body</h1>
+                <h2>An expression of care</h2>
+                <p>
+                  Aesop formulations offer the body deserving care, to cleanse,
+                  replenish, and nourish skin. Each product is a sensory
+                  pleasure to use with its own delightful aroma.
+                </p>
               </div>
               <div class="col-7">
-            <CardGroup>
-              <Card>
-                <Card.Img variant="top" src={require("../assets/images/daily_products/Aesop-Body-A-Rose-By-Any-Other-Name-Body-Cleanser-500mL-large.png")}  />
-                <Card.Body>
-                  <Card.Title>Rind Concentrate Body Balm</Card.Title>
-                  <Card.Text>
-                  A citrus-based formulation with oils of Orange and Lemon Rind to cool and refresh the skin, and nut oil extracts to soften and lightly hydrate.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-              <Card>
-                <Card.Img variant="top" src={require("../assets/images/daily_products/Aesop-Body-Geranium-Leaf-Body-Scrub-180mL-large.png")} />
-                <Card.Body>
-                  <Card.Title>A Rose By Any Other Name Body Cleanser</Card.Title>
-                  <Card.Text>
-                  An aromatic formulation that cleanses gently yet thoroughly, enhanced with hydrating Rose Petal oil and other skin-softening botanical extracts.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-              <Card>
-                <Card.Img variant="top" src={require("../assets/images/daily_products/Aesop-Body-Geranium-Leaf-Body-Cleanser-500mL-large.png")} />
-                <Card.Body>
-                  <Card.Title>Geranium Leaf Body Cleanser</Card.Title>
-                  <Card.Text>
-                  An invigorating gentle gel cleanser formulated with oils of Mandarin and Bergamot Rind to banish grime and thoroughly refresh the skin.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </CardGroup>
-            </div>
+                <CardGroup>
+                  <Card>
+                    <Card.Img
+                      variant="top"
+                      src={require("../assets/images/daily_products/Aesop-Body-A-Rose-By-Any-Other-Name-Body-Cleanser-500mL-large.png")}
+                    />
+                    <Card.Body>
+                      <Card.Title>Rind Concentrate Body Balm</Card.Title>
+                      <Card.Text>
+                        A citrus-based formulation with oils of Orange and Lemon
+                        Rind to cool and refresh the skin, and nut oil extracts
+                        to soften and lightly hydrate.
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                  <Card>
+                    <Card.Img
+                      variant="top"
+                      src={require("../assets/images/daily_products/Aesop-Body-Geranium-Leaf-Body-Scrub-180mL-large.png")}
+                    />
+                    <Card.Body>
+                      <Card.Title>
+                        A Rose By Any Other Name Body Cleanser
+                      </Card.Title>
+                      <Card.Text>
+                        An aromatic formulation that cleanses gently yet
+                        thoroughly, enhanced with hydrating Rose Petal oil and
+                        other skin-softening botanical extracts.
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                  <Card>
+                    <Card.Img
+                      variant="top"
+                      src={require("../assets/images/daily_products/Aesop-Body-Geranium-Leaf-Body-Cleanser-500mL-large.png")}
+                    />
+                    <Card.Body>
+                      <Card.Title>Geranium Leaf Body Cleanser</Card.Title>
+                      <Card.Text>
+                        An invigorating gentle gel cleanser formulated with oils
+                        of Mandarin and Bergamot Rind to banish grime and
+                        thoroughly refresh the skin.
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                </CardGroup>
+              </div>
             </div>
           </div>
 
